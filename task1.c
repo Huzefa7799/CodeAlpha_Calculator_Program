3d113237046281519436ad2b2209650af4e83547
Huzefa <huzefashaikh385@gmail.com>
2026-01-31 12:57:50 +0530

    Calculator Program
    C program written to perform basic arithmetic operations.

---
+++ task1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &op);   // space before %c to ignore newline
+
+    switch (op) {
+        case '+':
+            printf("Result = %.2f", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result = %.2f", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result = %.2f", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result = %.2f", num1 / num2);
+            else
+                printf("Error: Division by zero not allowed!");
+            break;
+
+        default:
+            printf("Invalid operation!");
+    }
+
+    return 0;
+}
